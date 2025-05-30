1e736cc33bdbf14ff7b487943a452f5583d8040f
swathi-blueto <swathi@myblueto.com>
2025-05-30 09:31:53 +0530

    updated code

--- Backend/server.js
+++ Backend/server.js
@@ -8,32 +8,27 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import friendRoutes from "./routes/friendRoutes.js";
-import chatRoutes from "./routes/chatRoutes.js"
-import notificationRoutes from "./routes/notificationRoutes.js"
-
-import adminRoutes from "./routes/adminRoutes.js"
+import chatRoutes from "./routes/chatRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-
-
 
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/friends", friendRoutes);
-app.use("/api/chats",chatRoutes);
-app.use("/api/admin",adminRoutes)
-app.use("/api/notifications",notificationRoutes)
+app.use("/api/chats", chatRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/notifications", notificationRoutes);
 
-
-
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
